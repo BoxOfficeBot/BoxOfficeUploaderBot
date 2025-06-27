b85a6febc991bc378d5f920994164290ba510795
BoxOfficeBot <me.sml@gmx.de>
2025-06-27 14:02:38 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,13 +3,13 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("files.db")
     c = conn.cursor()
-    c.execute('''
+    c.execute("""
         CREATE TABLE IF NOT EXISTS files (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             file_id TEXT NOT NULL,
-            file_name TEXT
+            file_name TEXT NOT NULL
         )
-    ''')
+    """)
     conn.commit()
     conn.close()
 
@@ -18,14 +18,14 @@ def save_file(file_id, file_name):
     c = conn.cursor()
     c.execute("INSERT INTO files (file_id, file_name) VALUES (?, ?)", (file_id, file_name))
     conn.commit()
-    row_id = c.lastrowid
+    file_row_id = c.lastrowid
     conn.close()
-    return row_id
+    return file_row_id
 
 def get_file(file_row_id):
     conn = sqlite3.connect("files.db")
     c = conn.cursor()
-    c.execute("SELECT file_id, file_name FROM files WHERE id = ?", (file_row_id,))
+    c.execute("SELECT file_id, file_name FROM files WHERE id=?", (file_row_id,))
     result = c.fetchone()
     conn.close()
     return result
