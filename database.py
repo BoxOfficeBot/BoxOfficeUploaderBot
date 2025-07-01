eb49e1bc20106e69dcd61a8381daec949d20e332
BoxOfficeBot <me.sml@gmx.de>
2025-07-01 20:05:48 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,12 +3,14 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("files.db")
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS files (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        file_id TEXT NOT NULL,
-        file_name TEXT,
-        file_type TEXT
-    )''')
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS files (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            file_id TEXT NOT NULL,
+            file_name TEXT,
+            file_type TEXT
+        )
+    ''')
     conn.commit()
     conn.close()
 
